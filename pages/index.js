ef26700f0e759426862c7b8df900a931f46680d1
jmamorteg <jmamorteg@gmail.com>
2022-05-15 16:48:06 +0200

    under construction published

--- pages/index.js
+++ pages/index.js
@@ -7,41 +7,56 @@ export default class Home extends Component {
     let { title, section } = attributes;
     return (
       <>
-        <Head>
-          <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
-        </Head>
-        <div className="tempHome" 
-             style={{background-color:black;}}>
-          <img className="logo" 
-               style={
-                 position: absolute;
-                 width: 50%;
-                 left: calc(50% - 509px/2 + 0.5px);
-                 top: 257px;
-                 } 
-               src="./public/img/RN_logo.svg" />
-          <p className="underConst"
-             style={
-
-             }>EN CONSTRUCCIÓN</p>
-          <p className="contact"
-             style={
-               
-             }>para más información contactar al +34 654 10 77 98</p>
+        <div style={{
+          position: "relative",
+          backgroundColor: "#fffdfa",
+          width: "100%",
+          height: "100vh"
+        }}>
+          <Head>
+            <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+            <link href="https://fonts.googleapis.com/css2?family=Maitree:wght@200&display=swap" rel="stylesheet" /> 
+            <link rel="stylesheet" href="https://use.typekit.net/ssl2wkl.css" />
+          </Head>
+          <div className="tempHome" 
+               style={{
+                 position: "absolute",
+                 left: "calc(50% - 509px/2 + 0.5px)",
+                 top: "calc(50% - 509px/2 + 0.5px)",
+               }}>
+            <img className="logo"
+                 style={{
+                   width: "509px",
+                   height: "509px",
+                 }} src="./img/RN_logo.svg" />
+            <p className="underConst"
+               style={{
+                 marginLeft: "125px",
+                 fontFamily: "'matrix-ii', serif",
+                 fontWeight: "300",
+                 fontStyle: "italic"
+               }}>mi página está en construcción</p>
+            <p className="contact"
+               style={{
+                 marginLeft: "125px",
+                 fontFamily: "'Maitree', serif"
+               }}>por ahora me puedes encontrar en <a href="https://www.instagram.com/raquel.neriz/" >Instagram</a> </p>
+          </div>
+          <article className="article" style={{visibility: "hidden"}}>
+            <h1>{title}</h1>
+            <HomeContent />
+            <ul>
+              {section.map((section, k) => (
+                <li key={k}>
+                  <h2>{section.name}</h2>
+                  <p>{section.description}</p>
+                </li>
+              ))}
+            </ul>
+          </article>
         </div>
-        <article className="article"
-                 style={visibility:hidden;}>
-          <h1>{title}</h1>
-          <HomeContent />
-          <ul>
-            {section.map((section, k) => (
-              <li key={k}>
-                <h2>{section.name}</h2>
-                <p>{section.description}</p>
-              </li>
-            ))}
-          </ul>
-        </article>
       </>
     )
   }
